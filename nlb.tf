1f41b35ef36a9b54d2525a6664539a506903cfcc
Brian Brookman <bcbrookman@users.noreply.github.com>
2025-05-04 22:39:51 -0700

    feat: add a network load balancer for the apiserver

---
+++ nlb.tf
@@ -0,0 +1,50 @@
+data "aws_subnet" "selected" {
+  id = var.subnet_id
+}
+
+resource "aws_lb" "apiserver" {
+  name               = var.cluster_name
+  load_balancer_type = "network"
+  internal           = true
+
+  subnet_mapping {
+    subnet_id = var.subnet_id
+  }
+}
+
+resource "aws_lb_target_group" "apiserver" {
+  name        = "${var.cluster_name}-tg"
+  port        = var.apiserver_port
+  protocol    = "TCP"
+  vpc_id      = data.aws_subnet.selected.vpc_id
+  target_type = "instance"
+
+  # Mitigate host RPF filtering when src_ip = dest_ip
+  preserve_client_ip = false
+
+  depends_on = [
+    aws_lb.apiserver
+  ]
+}
+
+resource "aws_lb_target_group_attachment" "apiserver" {
+  for_each = {
+    for k, v in module.controlplane-nodes :
+    k => v
+  }
+
+  target_group_arn = aws_lb_target_group.apiserver.arn
+  target_id        = each.value.id
+  port             = var.apiserver_port
+}
+
+resource "aws_lb_listener" "apiserver" {
+  load_balancer_arn = aws_lb.apiserver.arn
+  protocol          = "TCP"
+  port              = var.apiserver_port
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.apiserver.arn
+  }
+}
