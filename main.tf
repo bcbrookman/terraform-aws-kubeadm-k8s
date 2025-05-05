9061edf41831f958f09a59181cefda1ff0f6fbf2
Brian Brookman <bcbrookman@users.noreply.github.com>
2025-05-05 13:27:46 -0700

    feat: add input variable validations

--- main.tf
+++ main.tf
@@ -1,26 +1,31 @@
 locals {
-  apiserver_dns = var.apiserver_dns == "CLUSTERNAME-RANDOM.elb.REGION.amazonaws.com" ? null : var.apiserver_dns
+  # Input variable transforms
+  apiserver_dns            = var.apiserver_dns == "CLUSTERNAME-RANDOM.elb.REGION.amazonaws.com" ? "" : var.apiserver_dns
+  containerd_version       = var.containerd_version == "auto" ? "" : var.containerd_version
+  kubernetes_version       = var.kubernetes_version == "auto" ? "" : var.kubernetes_version
+  controlplane_node_ami_id = var.controlplane_node_ami_id == "auto" ? data.aws_ami.debian12.id : var.controlplane_node_ami_id
+  worker_node_ami_id       = var.worker_node_ami_id == "auto" ? data.aws_ami.debian12.id : var.worker_node_ami_id
+  ssh_key_name             = var.ssh_key_name == "none" ? "" : var.ssh_key_name
 }
 
 module "controlplane-nodes" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "5.7.1"
 
-  ami                  = var.controlplane_node_ami_id == null ? data.aws_ami.debian12.id : var.controlplane_node_ami_id
+  ami                  = local.controlplane_node_ami_id
   count                = var.controlplane_node_count
   iam_instance_profile = aws_iam_instance_profile.node_instance_profile.name
   instance_type        = var.controlplane_node_instance_type
-  key_name             = var.ssh_key_name
+  key_name             = local.ssh_key_name
   name                 = format("%s%02s", "${var.cluster_name}-cp", count.index + 1)
   subnet_id            = var.subnet_id
 
   user_data_base64 = base64encode(templatefile("${path.module}/user_data.sh.tftpl", {
     apiserver_dns           = coalesce(local.apiserver_dns, aws_lb.apiserver.dns_name)
     apiserver_port          = var.apiserver_port
-    containerd_version      = var.containerd_version
+    containerd_version      = local.containerd_version
     first_controlplane_node = count.index == 0 ? "true" : ""
-    kubernetes_version      = var.kubernetes_version
-    kubernetes_version      = var.kubernetes_version
+    kubernetes_version      = local.kubernetes_version
     node_role               = "control-plane"
     s3_bucket_name          = aws_s3_bucket.kubeadm_cmds.id
   }))
@@ -34,21 +39,20 @@ module "worker-nodes" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "5.7.1"
 
-  ami                  = var.worker_node_ami_id == null ? data.aws_ami.debian12.id : var.worker_node_ami_id
+  ami                  = local.worker_node_ami_id
   count                = var.worker_node_count
   iam_instance_profile = aws_iam_instance_profile.node_instance_profile.name
   instance_type        = var.worker_node_instance_type
-  key_name             = var.ssh_key_name
+  key_name             = local.ssh_key_name
   name                 = format("%s%02s", "${var.cluster_name}-wk", count.index + 1)
   subnet_id            = var.subnet_id
 
   user_data_base64 = base64encode(templatefile("${path.module}/user_data.sh.tftpl", {
     apiserver_dns           = coalesce(local.apiserver_dns, aws_lb.apiserver.dns_name)
     apiserver_port          = var.apiserver_port
-    containerd_version      = var.containerd_version
+    containerd_version      = local.containerd_version
     first_controlplane_node = ""
-    kubernetes_version      = var.kubernetes_version
-    kubernetes_version      = var.kubernetes_version
+    kubernetes_version      = local.kubernetes_version
     node_role               = "worker"
     s3_bucket_name          = aws_s3_bucket.kubeadm_cmds.id
   }))
--- variables.tf
+++ variables.tf
@@ -1,81 +1,141 @@
-variable "apiserver_crosszone_lb" {
+variable "apiserver_crosszone_lb_enabled" {
   description = "Whether cross-zone load balancing is enabled on the apiserver load balancer."
   type        = bool
   default     = false
+  nullable    = false
 }
 
 variable "apiserver_dns" {
-  description = "Overrides the DNS name used in kubeadm init/join commands. Use only when [customizing the apiserver DNS name]()."
+  description = "Overrides the DNS name used in kubeadm init/join commands. Use when [customizing the apiserver DNS name](https://registry.terraform.io/modules/bcbrookman/kubeadm-k8s/aws/auto#customizing-the-api-server-dns-name)."
   type        = string
   default     = "CLUSTERNAME-RANDOM.elb.REGION.amazonaws.com"
+  nullable    = false
+
+  validation {
+    condition = can(regex("^[A-Za-z0-9]([A-Za-z0-9](-[A-Za-z0-9]){0,61})*(\\.[A-Za-z0-9]([A-Za-z0-9](-[A-Za-z0-9]){0,61})*.)*\\.*$", var.apiserver_dns))
+    error_message = "The DNS name must be a valid DNS name like 'example.com' or 'sub.example.org'."
+  }
 }
 
 variable "apiserver_port" {
-  description = "The listening port used by the apiserver local instances and load balancer."
+  description = "The TCP listening port used by the apiserver local instances and load balancer."
   type        = number
   default     = 6443
+  nullable    = false
+
+  validation {
+    condition     = var.apiserver_port >= 1 && var.apiserver_port <= 65535
+    error_message = "Must be a port number between 1 and 65535."
+  }
 }
 
 variable "cluster_name" {
-  description = "The name of the cluster. Used in naming and labeling resources."
+  description = "The name of the cluster. Used in naming and labeling resources. Must be between 1 and 17 characters."
   type        = string
+  nullable    = false
+
+  validation {
+    condition     = length(var.cluster_name) >= 1 && length(var.cluster_name) <= 17
+    error_message = "Must be between 1 and 17 characters."
+  }
 }
 
 variable "containerd_version" {
-  description = "The version of containerd to *initially* install on nodes. Changing this variable after instance creation will **NOT** upgrade/downgrade containerd."
+  description = "The version of containerd to *initially* install on nodes. Changing this variable after instance creation will **NOT** upgrade/downgrade containerd. `\"auto\"` means the package manager should decide."
   type        = string
-  default     = ""
+  default     = "auto"
+  nullable    = false
+
+  validation {
+    condition = var.containerd_version == "auto" || can(regex(
+      "^\\d+\\.\\d+\\.\\d+(-[0-9A-Za-z-.]+)?(\\+[0-9A-Za-z-.]+)?$",
+      var.containerd_version
+    ))
+    error_message = "Must follow semantic versioning (e.g., 1.6.20, 1.6.20-beta.1, etc.)."
+  }
 }
 
 variable "controlplane_node_ami_id" {
-  description = "The AMI ID used to provision control-plane nodes."
+  description = "The AMI ID used to provision control-plane nodes. `\"auto\"` selects a Debian AMI."
   type        = string
-  default     = null
+  default     = "auto"
+  nullable    = false
 }
 
 variable "controlplane_node_count" {
-  description = "The number of control-plane nodes to provision."
+  description = "The number of control-plane nodes to provision. Must be 1 or more."
   type        = number
   default     = 1
+  nullable    = false
+
+  validation {
+    condition     = floor(var.controlplane_node_count) == var.controlplane_node_count && var.controlplane_node_count > 0
+    error_message = "Must be an integer greater than 0."
+  }
 }
 
 variable "controlplane_node_instance_type" {
-  description = "The instance type used to provision control-plane nodes (review the basic requirements at https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/provision-cluster-kubeadm/)."
+  description = "The instance type used to provision control-plane nodes (e.g. t2.micro, m5.large, etc.). Review the basic [node requirements](https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/create-cluster-kubeadm/#before-you-begin)."
   type        = string
   default     = "t3.small"
+  nullable    = false
 }
 
 variable "kubernetes_version" {
-  description = "The version of Kubernetes to *initially* intall on nodes. Changing this variable after instance creation will **NOT** upgrade/downgrade Kubernetes."
+  description = "The version of Kubernetes to *initially* install on nodes. Changing this variable after instance creation will **NOT** upgrade/downgrade Kubernetes. `\"auto\"` means the package manager should decide."
   type        = string
-  default     = ""
+  default     = "auto"
+  nullable    = false
+
+  validation {
+    condition = var.kubernetes_version == "auto" || can(regex(
+      "^\\d+\\.\\d+\\.\\d+(-[0-9A-Za-z-.]+)?(\\+[0-9A-Za-z-.]+)?$",
+      var.kubernetes_version
+    ))
+    error_message = "Must follow semantic versioning (e.g., 1.31.0, 1.31.0-beta.1, etc.)."
+  }
 }
 
 variable "ssh_key_name" {
-  description = "The name of an existing Key Pair for connecting to instances. Often used with [`aws_key_pair.<name>.key_name`](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/key_pair)."
+  description = "The name of an existing Key Pair for connecting to instances. Can be set with [`aws_key_pair.<name>.key_name`](https://registry.terraform.io/providers/hashicorp/aws/auto/docs/resources/key_pair). `\"none\"` means no Key Pair will be attached to the instances."
   type        = string
-  default     = null
+  default     = "none"
+  nullable    = false
 }
 
 variable "subnet_id" {
-  description = "The subnet ID to create the cluster nodes in."
+  description = "The subnet ID to create the cluster nodes in. Can be set with [`aws_key_pair.<name>.key_name`](https://registry.terraform.io/providers/hashicorp/aws/auto/docs/resources/key_pair)."
   type        = string
+  nullable    = false
 }
 
 variable "worker_node_ami_id" {
-  description = "The AMI ID used to provision worker nodes"
+  description = "The AMI ID used to provision worker nodes. `\"auto\"` selects a Debian AMI."
   type        = string
-  default     = null
+  nullable    = false
+  default     = "auto"
 }
 
 variable "worker_node_count" {
-  description = "The number of worker nodes to provision."
+  description = "The number of worker nodes to provision. Must be 0 or more."
   type        = number
   default     = 1
+  nullable    = false
+
+  validation {
+    condition     = floor(var.worker_node_count) == var.worker_node_count && var.worker_node_count >= 0
+    error_message = "Must be an integer greater than or equal to 0."
+  }
 }
 
 variable "worker_node_instance_type" {
-  description = "The instance type used to provision worker nodes."
+  description = "The instance type used to provision worker nodes (e.g. t2.micro, m5.large, etc.)."
   type        = string
   default     = "t3.small"
+  nullable    = false
+
+  validation {
+    condition     = can(regex("^[a-z0-9]+\\.[a-z0-9]+$", var.worker_node_instance_type))
+    error_message = "Must be an instance type in the format t2.micro, m5.large, etc."
+  }
 }
